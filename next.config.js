c1a000922c778fcd346d104a756336156b08717c
vincentochoa3 <vochoa893@gmail.com>
2022-11-17 13:11:11 -0800

    add header image and domains to config file

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ["links.papareact.com", "fakestoreapi.com"]
+  },
   reactStrictMode: true,
   swcMinify: true,
 }
