2bb52e5e74b4467e4e24132e25d5256ae32a4dab
vincentochoa3 <vochoa893@gmail.com>
2022-11-18 15:24:04 -0800

    add the product component

--- components/ProductFeed.js
+++ components/ProductFeed.js
@@ -1,12 +1,21 @@
+import Product from "./Product"
+
 function ProductFeed({ products }) {
   return (
     <div>
-      <h1>Products here...</h1>
       {products.map(({ id, title, price, description, category, image }) =>
-        <p>{title}</p>
+        <Product
+          key={id}
+          id={id}
+          title={title}
+          price={price}
+          description={description}
+          category={category}
+          image={image}
+        />
       )}
     </div>
   )
 }
 
-export default ProductFeed
+export default ProductFeed;
