c4c6898c010826d0d13b5669552e9b36c0747d64
vincentochoa3 <vochoa893@gmail.com>
2022-11-18 13:17:38 -0800

    add use of getserversideprops to fetch from fake store api

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import Header from "../components/Header"
 import Banner from "../components/Banner"
+import ProductFeed from "../components/ProductFeed"
 
-export default function Home() {
+export default function Home({ products }) {
   return (
     <div className="bg-gray-100">
       <Head>
@@ -14,9 +14,23 @@ export default function Home() {
 
       <Header />
 
+
       <main className="max-w-screen-2xl mx-auto">
         <Banner />
+        <ProductFeed products={products} />
       </main>
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  const products = await fetch("https://fakestoreapi.com/products").then(
+    (res) => res.json()
+  )
+
+  return {
+    props: {
+      products,
+    }
+  }
 }
