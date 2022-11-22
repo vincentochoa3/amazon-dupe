005c793a8fb42f9fdc6d8a9482e5d4b1198b6c16
vincentochoa3 <vochoa893@gmail.com>
2022-11-22 12:39:12 -0800

    add routing for chekcout and home page

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,18 @@
 import Image from "next/image";
 import { MenuIcon, SearchIcon, ShoppingCartIcon } from "@heroicons/react/outline"
 import { signIn, signOut, useSession } from "next-auth/react"
+import { useRouter } from "next/router"
 
 function Header() {
   const { data: session } = useSession()
+  const router = useRouter();
 
   return (
     <header>
       <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
         <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
           <Image
+            onClick={() => router.push("/")}
             src="https://links.papareact.com/f90"
             width={150}
             height={40}
@@ -32,7 +35,7 @@ function Header() {
             <p>Returns</p>
             <p className="font-extrabold md:text-sm">& Orders</p>
           </div>
-          <div className="relative link flex items-center">
+          <div className="relative link flex items-center" onClick={() => router.push("/checkout")} >
             <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">0</span>
             <ShoppingCartIcon className="h-10" />
             <p className="hidden md:inline font-extrabold md:text-sm mt-2">Basket</p>
---
+++ pages/checkout.js
@@ -0,0 +1,13 @@
+import Header from "../components/Header"
+
+function Checkout() {
+
+  return (
+    <div className="bg-gray-100">
+      <Header />
+      <h1>CHECKOUT</h1>
+    </div>
+  )
+}
+
+export default Checkout
