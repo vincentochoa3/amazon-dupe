775204bf2220682925e7bc3701f6fd4c366a1f3c
vincentochoa3 <vochoa893@gmail.com>
2022-11-22 11:36:12 -0800

    add firebase config and install next auth and firebase

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { MenuIcon, SearchIcon, ShoppingCartIcon } from "@heroicons/react/outline"
+import { signIn, signOut, useSession } from "next-auth/react"
 
 function Header() {
   return (
@@ -19,7 +20,7 @@ function Header() {
           <SearchIcon className="h-12 p-4" />
         </div>
         <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
-          <div className="link">
+          <div className="link" onClick={signIn}>
             <p>Hello Vincent</p>
             <p className="font-extrabold md:text-sm">Account & Lists</p>
           </div>
---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDA8E3DXtm78cdBmclvR5-DEkNbU6YgrbQ",
+  authDomain: "amzn-dupe.firebaseapp.com",
+  projectId: "amzn-dupe",
+  storageBucket: "amzn-dupe.appspot.com",
+  messagingSenderId: "668426305698",
+  appId: "1:668426305698:web:849d9a8c159d2ba1b517fb"
+};
