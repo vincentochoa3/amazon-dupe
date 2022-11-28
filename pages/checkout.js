5e2c67420861b7e08ce17be6ab4b46e49c8ec45d
vincentochoa3 <vochoa893@gmail.com>
2022-11-28 13:22:39 -0800

    add basic ui for checkout page

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,11 +1,24 @@
-import Header from "../components/Header"
+import Image from "next/image";
+import Header from "../components/Header";
 
 function Checkout() {
 
   return (
     <div className="bg-gray-100">
       <Header />
-      <h1>CHECKOUT</h1>
+      <main className="lg:flex max-w-screen-2xl mx-auto">
+        <div className="flex-grow m-5 shadow-sm">
+          <Image
+            src="https://links.papareact.com/ikj"
+            width={1020}
+            height={250}
+            objectFit="contain"
+          />
+          <div className="flec flex-col p-5 space-y-10 bg-white">
+            <h1 className="text-3xl border-b pb-4">Your Shopping Basket</h1>
+          </div>
+        </div>
+      </main>
     </div>
   )
 }
